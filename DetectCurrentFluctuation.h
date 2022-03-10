50f6232261ab1c482f7a04c0a1762ef546580008
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-03-10 10:48:09 +0530

    Create DetectCurrentFluctuation.h

---
+++ DetectCurrentFluctuation.h
@@ -0,0 +1,8 @@
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* qsort */
+
+#define MAX_NUMBER_OF_READINGS 50
+
+int minValues[MAX_NUMBER_OF_READINGS] = {0};
+int maxValues[MAX_NUMBER_OF_READINGS] = {0};
+int countValues[MAX_NUMBER_OF_READINGS] = {0};
