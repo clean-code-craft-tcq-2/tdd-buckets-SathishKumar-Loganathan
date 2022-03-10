d4db07e6c4a39fd7711af5e03335590782774d5c
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-03-10 10:48:45 +0530

    Update DetectCurrentFluctuation.h

--- DetectCurrentFluctuation.h
+++ DetectCurrentFluctuation.h
@@ -3,6 +3,6 @@
 
 #define MAX_NUMBER_OF_READINGS 50
 
-int minValues[MAX_NUMBER_OF_READINGS] = {0};
-int maxValues[MAX_NUMBER_OF_READINGS] = {0};
-int countValues[MAX_NUMBER_OF_READINGS] = {0};
+int minValues[MAX_NUMBER_OF_READINGS];
+int maxValues[MAX_NUMBER_OF_READINGS];
+int countValues[MAX_NUMBER_OF_READINGS];
