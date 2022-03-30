53f0a95eef7614b3cb5350521742e81c5cff402e
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-03-30 09:56:12 +0530

    Update Test_CurrentFluctuation.cpp

--- Test_CurrentFluctuation.cpp
+++ Test_CurrentFluctuation.cpp
@@ -4,6 +4,6 @@
 #include "DetectCurrentFluctuation.h"
 
 TEST_CASE("Checks No Of Range in given array") {
-  int arr[] = {3, 3, 5, 4, 10, 11, 12};
-  REQUIRE(Capture_and_Print_Current_values(arr,sizeof(arr)/sizeof(arr[0])) == 2);
+  int arr[] = {0, 10, 50, 100, 200, 2123, 2743, 3521, 4095, 4095, 4094, 4000, 0, 1010};
+  REQUIRE(Capture_and_Print_Current_values(arr,sizeof(arr)/sizeof(arr[0])) == 4);
 }
